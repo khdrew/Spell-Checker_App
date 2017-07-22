d0db86a4af7d5be9b58e4e67d8c971d9f23b68fc
khdrew <andrewex.lai@hotmail.com>
2017-07-22 22:22:45 +1200

    added files

---
+++ MyCognitiveApp/MyCognitiveApp/FlaggedTokens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCognitiveApp
+{
+    public class FlaggedTokens
+    {
+        public int Offset { get; set; }
+        public string Token { get; set; }
+        public string Type { get; set; }
+        public IList<SpellCheckSuggestion> Suggestions { get; set; }
+    }
+}
